ce92e9807e6ae3c7a69bcc6bfab6299d0a8970c6
Axel Fiolle <ax.fiolle@gmail.com>
2019-03-18 23:28:44 +0100

    removed useless

--- src/pages/main.js
+++ src/pages/main.js
@@ -8,21 +8,12 @@ class MainPage extends PolymerElement {
                 #site {
                     display: none;
                 }
-                #site.show-website-true {
-                    display: initial;
-                }
+                #site.show-website-true {display: initial;}
+
+                .hidden {display: none;}
             </style>
             <main id="site" class$="show-website-[[showWebsite]]">
                 <h1 id="title" class="hidden">Axel Fiolle</h1>
-                <nav id="accessibility" class="hidden">
-                    <ul id="menu">
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#portfolio">Portfolio</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                    </ul>
-                </nav>
-
                 <section id="home">
                     <div class="discover"><span id="jsFill"></span><h2 id="jobs">FrontEnd-Dev</h2></div>
                 </section>
